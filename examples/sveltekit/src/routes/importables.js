575076bb70b536511996b7ad470f49ebe7f6d785
Doug Anderson444 <douganderson444@gmail.com>
2024-01-29 09:09:44 -0400

    bump JS to wurbo 1.8

--- examples/sveltekit/src/routes/importables.js
+++ examples/sveltekit/src/routes/importables.js
@@ -1,20 +1,29 @@
 export function buildCodeString(namespace) {
 	return `
       const bc = new BroadcastChannel('${namespace}');
-
       export function addeventlistener({ selector, ty }) {
         document.querySelector(selector).addEventListener(ty, (e) => {
+
+          let val = e.target.value;
 
           // detect if form event
           if(e.target.closest('form')) {
             e.preventDefault();
           }
 
-          let val = (e.target.dataset.contextValue && e.target.value)
-             ? { ctx: JSON.parse(e.target.dataset.contextValue), value: e.target.value }
-             : e.target.dataset.contextValue || e.target.value;
-          
-          let ctx = { tag: e.target.dataset.contextName || e.target.name, val };
+          let tag  = e.target.dataset.contextName || e.target.name;
+
+          try {
+            val = Object.assign({}, 
+                    typeof JSON.parse(e.target.dataset.contextValue) === 'object' 
+                    ? JSON.parse(e.target.dataset.contextValue) 
+                    : {}, 
+                    { value: e.target.value });
+          } catch(e) {
+            console.warn('Could not parse contextValue');
+          }
+
+          let ctx = { tag, val };
 
           let el = e.target.closest('[data-slot]');
           if(el) {
@@ -22,14 +31,8 @@ export function buildCodeString(namespace) {
             el = el.closest('[data-slot]');
           }
 
-          if(!e.target.dataset.contextTarget) {
-            console.warn("No contextTarget found on event target. Did you set data-context-target='output.html'?");
-            return
-          }
+          let rendered = window.${namespace}.render(ctx); 
 
-          let rendered = window.${namespace}.render(ctx, e.target.dataset.contextTarget); 
-
-          // console.log({ctx}, {rendered});
           bc.postMessage(rendered);
         });
       }
